dae1d8fea6b6e8b869bddb0a2f4052d680318730
codingjwp <codingjwp@gmail.com>
2023-10-08 05:01:21 +0900

    feat: create to mobile image slider

--- imagepage/js/action.js
+++ imagepage/js/action.js
@@ -1,5 +1,11 @@
-const btn_menu = document.querySelector('#btn_menu');
-// const gnb = document.querySelector('#gnb');
+const btn_menu = document.querySelector('#btnMenu');
+const gnb = document.querySelector('#gnb');
+const left = document.querySelector('#leftArrow');
+const right = document.querySelector('#rightArrow');
+const sliders = document.querySelectorAll('.slider .wrap a');
+
+let sliderIndex = 0;
+const totalSlider = sliders.length;
 
 function menuClickEvent() {
   if (btn_menu.classList.contains('close')) {
@@ -22,4 +28,26 @@ function menuClickEvent() {
   }
 }
 
+const sliderPositionChange = () => {
+  let count = 0;
+  for (let slide of sliders) {
+    if (!slide.classList.contains('hide'))
+      slide.classList.add('hide')
+    if (count === sliderIndex)
+      slide.classList.remove('hide');
+    count++;
+  }
+}
+
+const sliderLeftMoveClick = () => {
+  sliderIndex = (sliderIndex === 0 ? totalSlider - 1 : sliderIndex - 1);
+  sliderPositionChange();
+}
+const sliderRightMoveClick = () => {
+  sliderIndex = (sliderIndex === totalSlider - 1 ? 0 : sliderIndex + 1);
+  sliderPositionChange();
+}
+
 btn_menu.addEventListener('click', menuClickEvent);
+left.addEventListener('click', sliderLeftMoveClick);
+right.addEventListener('click', sliderRightMoveClick);
