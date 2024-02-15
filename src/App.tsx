04045282efbc6f06f8dae24de2eef96e52b572b6
codingjwp <codingjwp@gmail.com>
2024-02-15 17:05:52 +0900

    feat: json-server 생성

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import routerConfig from './route';
-import {RouterProvider} from 'react-router-dom';
-import './index.css';
-
-function App() {
-  return <RouterProvider router={routerConfig} />;
-}
-
-export default App;
