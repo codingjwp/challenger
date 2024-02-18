99a640381b303706ef89339f118b15f54df21b5b
codingjwp <codingjwp@gmail.com>
2024-02-19 01:20:34 +0900

    feat: backend 챌린지  delete put get 생성

--- server.js
+++ server.js
@@ -4,52 +4,50 @@ import { Low } from 'lowdb'
 import { JSONFile } from 'lowdb/node';
 import bcypt from 'bcrypt';
 import Joi from 'joi';
-import fs, { stat } from 'fs';
-import { title } from 'process';
+import fs from 'fs';
 
 const INITALDB = {
   challengeImg: {
-    maxLength: 8,
     list: [
       {
         id: 'challenge_img_01',
-        imgSrc: 'assets/cooking.png',
+        imgSrc: '/assets/cooking.png',
         imgAlt: 'A man is cooking',
         type: 'cooking',
       },
       {
         id: 'challenge_img_02',
-        imgSrc: 'assets/exercises.png',
+        imgSrc: '/assets/exercises.jpg',
         imgAlt: 'A woman is doing yoga',
         type: 'exercises',
       },
       {
         id: 'challenge_img_03',
-        imgSrc: 'assets/investment.png',
+        imgSrc: '/assets/investment.png',
         imgAlt: 'Money and graphs with a piggy bank',
         type: 'investment',
       },
       {
         id: 'challenge_img_04',
-        imgSrc: 'assets/pets.png',
+        imgSrc: '/assets/pets.png',
         imgAlt: 'Bunny holding a heart',
         type: 'pets',
       },
       {
         id: 'challenge_img_05',
-        imgSrc: 'assets/programmers.png',
+        imgSrc: '/assets/programmers.png',
         imgAlt: 'A man programming ',
         type: 'programmers',
       },
       {
         id: 'challenge_img_06',
-        imgSrc: 'assets/readings.png',
+        imgSrc: '/assets/readings.png',
         imgAlt: 'Man reading, sitting on a book ',
         type: 'readings',
       },
       {
         id: 'challenge_img_07',
-        imgSrc: 'assets/studys.png',
+        imgSrc: '/assets/studys.png',
         imgAlt: 'Study room',
         type: 'studys',
       },
@@ -89,8 +87,8 @@ server.use(jsonServer.bodyParser);
 server.post('/signin', async (req, res, next) => {
   try {
     const { nick, password } = req.body;
-    const signup = await validateLogin(nick, password, password);
-    const data = users.info.filter((info) => info.nick == signup.nick);
+    const signin = await validateLogin(nick, password, password);
+    const data = users.info.filter((info) => info.nick == signin.nick);
     if (!data) {
       const { status, message } = errorMessage(400, "일치하는 닉네임이 없습니다.");
       res.status(400).send({ status, message });
@@ -124,7 +122,14 @@ server.post('/signin', async (req, res, next) => {
 server.post('/signup', async (req, res, next) => {
   try {
     const { nick, password, confirm } = req.body;
+    const isNick = users.info.find((user) => user.nick === nick);
+    if (isNick) {
+      const { status, message } = errorMessage(400, "이미 존재하는 닉네임 입니다.");
+      res.status(400).send({ status, message });
+      return;
+    }
     const signup = await validateLogin(nick, password, confirm);
+    // nickname과 페스워드같은 아이뒤가 존재할 경우 에러 발생
     const id = uuid();
     await db.update(({ users }) =>
       users.info.push({
@@ -156,16 +161,102 @@ server.get('/challenge', (req, res, next) => {
     }
     const posts = findedUser.posts.filter((post) => post.status === status);
 
-    res.status(200).send({ posts: posts });
+    res.status(200).send({
+      challengeLength: findedUser.challengeLength,
+      sucessLength: findedUser.sucessLength,
+      failureLength: findedUser.failureLength,
+      posts: posts
+    });
   } catch (error) {
     next(error);
   }
 })
+
+
+// post 글 수정
+server.post('/challenge/:id', async (req, res, next) => {
+  try {
+    const postId = req.params.id;
+    const { userId, title, imgSrc, description, startDate, endDate } = req.body;
+    const findedUser = users.info.find((item) => item.userId === userId);
+    if (!findedUser) {
+      const { status, message } = errorMessage(400, "해당 유저를 찾지 못했습니다.");
+      res.status(400).send(status, message);
+      return;
+    }
+    const findedPost = findedUser.posts.find((post) => post.postId === postId);
+    if (!findedPost) {
+      const { status, message } = errorMessage(400, "해당 챌린지를 찾지 못했습니다.");
+      res.status(400).send(status, message);
+      return;
+    }
+    findedPost.title = title;
+    findedPost.imgSrc = imgSrc;
+    findedPost.description = description;
+    findedPost.startDate = startDate;
+    findedPost.endDate = endDate;
+    await db.write();
+    res.status(200).send({ status: 200, data: { message: `챌린지 변경이 완료되었습니다.` } })
+  } catch (error) {
+    next(error);
+  }
+})
+
+// post 성공 실패 수정
+server.put('/challenge', async (req, res, next) => {
+  try {
+    const { userId, postId, status } = req.body;
+    const findedUser = users.info.find((item) => item.userId === userId);
+    if (!findedUser) {
+      const { status, message } = errorMessage(400, "해당 유저를 찾지 못했습니다.");
+      res.status(400).send(status, message);
+      return;
+    }
+    const findedPost = findedUser.posts.find((post) => post.postId === postId);
+    if (!findedPost) {
+      const { status, message } = errorMessage(400, "해당 챌린지를 찾지 못했습니다.");
+      res.status(400).send(status, message);
+      return;
+    }
+    const postStatus = findedPost.status;
+    if (postStatus === status) {
+      const { status, message } = errorMessage(400, "동일한 챌린지 상태입니다.");
+      res.status(400).send(status, message);
+      return;
+    }
+    if (postStatus === 'challenge') {
+      findedUser.challengeLength -= 1;
+      allChallenge.challengeLength -= 1;
+    } else if (postStatus === 'sucess') {
+      findedUser.sucessLength -= 1;
+      allChallenge.sucessLength -= 1;
+    } else {
+      findedUser.failureLength -= 1;
+      allChallenge.failureLength -= 1;
+    }
+
+    if (status === 'sucess') {
+      findedUser.sucessLength += 1;
+      allChallenge.sucessLength += 1;
+    } else if (status === 'failure') {
+      findedUser.failureLength += 1;
+      allChallenge.failureLength += 1;
+    }
+    findedPost.status = status;
+    await db.write();
+    res.status(200).send({ status: 200, data: { message: `챌린지를 ${status}로 변경 완료되었습니다.` } })
+
+  } catch (error) {
+    next(error);
+  }
+})
+
 // posts 생성
 server.post('/challenge', async (req, res, next) => {
   try {
-    const { userId, title, type, description, startDate, endDate } = req.body;
+    const { userId, title, imgSrc, description, startDate, endDate } = req.body;
     const findedUser = users.info.find((item) => item.userId === userId);
+
     if (!findedUser) {
       const { status, message } = errorMessage(400, "해당 유저를 찾지 못했습니다.");
       res.status(400).send(status, message);
@@ -176,7 +267,7 @@ server.post('/challenge', async (req, res, next) => {
     const newPosts = {
       postId: postId,
       title: title,
-      type: type,
+      imgSrc: imgSrc,
       description: description,
       startDate: startDate,
       endDate: endDate,
@@ -192,6 +283,54 @@ server.post('/challenge', async (req, res, next) => {
     next(error)
   }
 });
+
+//post 삭제
+server.delete('/challenge/:id', async (req, res, next) => {
+  try {
+    const postId = req.params.id;
+    const { userId } = req.body;
+
+    const findedUser = users.info.find((item) => item.userId === userId);
+    if (!findedUser) {
+      const { status, message } = errorMessage(400, "해당 유저를 찾지 못했습니다.");
+      res.status(400).send(status, message);
+      return;
+    }
+    const findedPost = findedUser.posts.find((post) => post.postId === postId);
+    if (!findedPost) {
+      const { status, message } = errorMessage(400, "해당 챌린지를 찾지 못했습니다.");
+      res.status(400).send(status, message);
+      return;
+    }
+    const findedStatus = findedPost.status;
+    if (findedStatus === 'challenge') {
+      findedUser.challengeLength -= 1;
+      allChallenge.challengeLength -= 1;
+    } else if (findedStatus === 'sucess') {
+      findedUser.sucessLength -= 1;
+      allChallenge.sucessLength -= 1;
+    } else {
+      findedUser.failureLength -= 1;
+      allChallenge.failureLength -= 1;
+    }
+    const updatePost = findedUser.posts.filter((post) => post.postId !== postId);
+    findedUser.posts = [...updatePost];
+    await db.write();
+    res.status(200).send({ status: 200, data: { message: "챌린지 삭제가 완료되었습니다." } });
+  } catch (error) {
+    next(error);
+  }
+})
+
+// challenge image 가져오는 부분
+server.get('/v2/image', (req, res, next) => {
+  try {
+    const { list } = challengeImg;
+    return res.status(200).send({ list });
+  } catch (error) {
+    next(error);
+  }
+})
 
 // 에러 넘김 부분
 server.use((error, req, res, next) => {
@@ -213,7 +352,7 @@ async function validateLogin(nick, password, confirm) {
   const schema = Joi.object({
     nick: Joi.string().alphanum().min(3).max(8).required(),
     password: Joi.string().min(8).max(20).pattern(new RegExp(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W]).+$/)).required(),
-    confirm: Joi.ref('password'),
+    confirm: Joi.any().valid(Joi.ref('password')).required(),
   });
   try {
     const saltRounds = 10;
@@ -222,7 +361,7 @@ async function validateLogin(nick, password, confirm) {
     return { nick: value.nick, password: hash };
   } catch (error) {
     if (error instanceof Error) {
-      throw new Error(error?.message || 'Failed vailed Error signup');
+      throw new Error("닉네임과 비밀번호를 다시 한번 확인해주세요.");
     }
     throw error;
   }
